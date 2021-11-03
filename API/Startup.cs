91e8a6b21ee4e2330d2987e20b32b1e624f1bc8e
DESKTOP-50U56HO <86040491+KamranIsmayilovv@users.noreply.github.com>
2021-11-03 14:29:34 +0400

    feat: create Data layer

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +28,10 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<DataContext>(options => 
+            {
+                options.UseSqlite("Connection string");
+            });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
