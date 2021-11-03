ab78f43ef6665c17a15c7306c86c7fdc802ed27b
DESKTOP-50U56HO <86040491+KamranIsmayilovv@users.noreply.github.com>
2021-11-03 13:40:16 +0400

    feat: create Entities layer

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
