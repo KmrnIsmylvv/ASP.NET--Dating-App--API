4cc6c62e45a07c514bb1830e9ac3f799643b9919
DESKTOP-50U56HO <86040491+KamranIsmayilovv@users.noreply.github.com>
2021-11-04 21:41:49 +0400

    fds

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot"
+             "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
