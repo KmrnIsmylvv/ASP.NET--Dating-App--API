97641a00fa7a37c376a7239f5fdf4d71dab55d68
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-11-06 20:21:30 +0400

    feat: create BaseApiController and inherited it

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -9,9 +9,8 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Controllers
 {
 
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+ 
+    public class UsersController :BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
