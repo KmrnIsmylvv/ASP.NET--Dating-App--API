ca17e425830153945d4188f4eaaea41bad16c66f
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-11-06 20:03:53 +0400

    refactor: update User Entity

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -3,6 +3,8 @@ namespace API.Entities
     public class AppUser
     {
         public int Id  { get; set; }
-        public string UserName { get; set; }        
+        public string UserName { get; set; }
+        public byte[] PasswordHash  { get; set; }
+        public byte[] PasswordSalt  { get; set; }        
     }
 }
