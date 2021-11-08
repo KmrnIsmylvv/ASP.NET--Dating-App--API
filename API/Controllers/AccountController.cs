4f4e5a868df517aec6ec238e12b99b10f91d7982
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-11-08 08:31:37 +0400

    feat: create LoginDto class and login operation

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Threading.Tasks;
 using API.Data;
 using API.Dtos;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -34,6 +35,25 @@ namespace API.Controllers
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto) 
+        {
+            var user =await _context.Users.SingleOrDefaultAsync(x =>x.UserName==loginDto.Username);
+
+            if(user==null) return Unauthorized("Invalid username");
+
+            using var hmac=new HMACSHA512(user.PasswordSalt);
+            
+            var computedHash=hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for(int i=0;i<computedHash.Length;i++)
+            {
+                if(computedHash[i]!=user.PasswordHash[i]) return Unauthorized("Invalid password");
+            }
 
             return user;
         }
