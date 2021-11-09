f6abae63d745430e65c4dedc43439e5feb9c41fb
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-11-09 19:20:36 +0400

    feat: create ITokenService interface and TokenService classs

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,13 @@
+using API.Entities;
+using API.Interfaces;
+
+namespace API.Services
+{
+    public class TokenService : ITokenService
+    {
+        public string CreateToken(AppUser user)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.Interfaces;
+using API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -28,6 +30,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<ITokenService,TokenService>();
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
