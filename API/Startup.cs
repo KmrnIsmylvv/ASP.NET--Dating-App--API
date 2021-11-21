1d2909aaa65ac7c21f61159c5c2f6d5be67c4b2a
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-11-21 14:03:43 +0400

    feat: creating error controller

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,10 +32,10 @@ namespace API
             services.AddIdentityServices(_config);
 
 
-            // services.AddSwaggerGen(c =>
-            // {
-            //     c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            // });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
