9ae3a6c496817c283cd513d08e2eacb67bc6b21b
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-11-24 14:39:08 +0400

    feat: create DateTime Extension class

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using API.Extensions;
 
 namespace API.Entities
 {
@@ -22,6 +23,10 @@ namespace API.Entities
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set;}
 
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
 
 
 
