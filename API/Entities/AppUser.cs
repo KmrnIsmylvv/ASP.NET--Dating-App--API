74c7817fbb4fab80bb6f4b091cf74737fdeddcbd
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-11-24 14:26:04 +0400

    feat: crate photo class and extend AppUser class

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace API.Entities
 {
     public class AppUser
@@ -5,6 +8,23 @@ namespace API.Entities
         public int Id  { get; set; }
         public string UserName { get; set; }
         public byte[] PasswordHash  { get; set; }
-        public byte[] PasswordSalt  { get; set; }        
+        public byte[] PasswordSalt  { get; set; }     
+
+        public DateTime DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; }=DateTime.Now;
+        public DateTime LastActive { get; set; }=DateTime.Now;
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set;}
+
+
+
+
+
     }
 }
--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -37,7 +37,7 @@ namespace API.Middleware
                 var response=_env.IsDevelopment()
                     ? new ApiException(context.Response.StatusCode,ex.Message,ex.StackTrace?.ToString())
                     : new ApiException(context.Response.StatusCode, "Internal Server Error");
-          
+                
                 var options=new JsonSerializerOptions{PropertyNamingPolicy=JsonNamingPolicy.CamelCase};
 
                 var json=JsonSerializer.Serialize(response,options);
