c31a9599300917df8437411abbb773dfcbf2b611
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-11-29 06:01:41 +0400

    feat: Adding AutoMapper

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.Helpers;
 using API.Interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
@@ -12,7 +13,8 @@ namespace API.Extensions
         public static IServiceCollection AddApplicationServices(this IServiceCollection services,IConfiguration config)
         {
             services.AddScoped<ITokenService,TokenService>();
-            services.AddScoped<IUserRepository,UserRepository>();   
+            services.AddScoped<IUserRepository,UserRepository>();  
+            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly); 
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(config.GetConnectionString("DefaultConnection"));
---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,15 @@
+using API.DTOs;
+using API.Entities;
+using AutoMapper;
+
+namespace API.Helpers
+{
+    public class AutoMapperProfiles: Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AppUser,MemberDto>();
+            CreateMap<Photo,PhotoDto>();
+        }
+    }
+}
