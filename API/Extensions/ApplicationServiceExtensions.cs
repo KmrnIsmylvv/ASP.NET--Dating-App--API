8c1cbeb887776a23efc9d700cc8eee03e4a511e4
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-12-07 14:06:23 +0400

    feat: added Photo service

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -8,14 +8,15 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace API.Extensions
 {
-    public  static class ApplicationServiceExtensions
+    public static class ApplicationServiceExtensions
     {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services,IConfiguration config)
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
             services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
-            services.AddScoped<ITokenService,TokenService>();
-            services.AddScoped<IUserRepository,UserRepository>();  
-            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly); 
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IPhotoService, PhotoService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(config.GetConnectionString("DefaultConnection"));
