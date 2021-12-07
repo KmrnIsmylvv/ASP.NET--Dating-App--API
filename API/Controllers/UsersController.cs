99967c50b9f85ebc0be8517b3eee584128c3276d
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-12-07 15:35:26 +0400

    feat: added CreatedAtRoute method

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -31,62 +31,64 @@ namespace API.Controllers
         }
 
 
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
-    {
-        var users = await _userRepository.GetMembersAsync();
-
-        return Ok(users);
-    }
-
-
-    [HttpGet("{username}")]
-    public async Task<ActionResult<MemberDto>> GetUser(string username)
-    {
-        return await _userRepository.GetMemberAsync(username);
-    }
-
-    [HttpPut]
-    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
-    {
-        var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
-
-        _mapper.Map(memberUpdateDto, user);
-
-        _userRepository.Update(user);
-
-        if (await _userRepository.SaveAllAsync()) return NoContent();
-
-        return BadRequest("Failed to update User");
-    }
-
-    [HttpPost("add-photo")]
-    public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
-    {
-        var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
-
-        var result=await _photoService.AddPhotoAsync(file);
-
-        if(result.Error!=null) return BadRequest(result.Error.Message);
-
-        var photo=new Photo
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-            Url=result.SecureUrl.AbsoluteUri,
-            PublicId=result.PublicId
-        };
+            var users = await _userRepository.GetMembersAsync();
 
-        if(user.Photos.Count==0)
-        {
-            photo.IsMain=true;
+            return Ok(users);
         }
 
-        user.Photos.Add(photo);
 
-        if(await _userRepository.SaveAllAsync())
-            return _mapper.Map<PhotoDto>(photo);
+        [HttpGet("{username}", Name = "GetUser")]
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
+        {
+            return await _userRepository.GetMemberAsync(username);
+        }
 
-        return BadRequest("Problem adding photo");
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
 
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user);
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update User");
+        }
+
+        [HttpPost("add-photo")]
+        public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
+        {
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+
+            var result = await _photoService.AddPhotoAsync(file);
+
+            if (result.Error != null) return BadRequest(result.Error.Message);
+
+            var photo = new Photo
+            {
+                Url = result.SecureUrl.AbsoluteUri,
+                PublicId = result.PublicId
+            };
+
+            if (user.Photos.Count == 0)
+            {
+                photo.IsMain = true;
+            }
+
+            user.Photos.Add(photo);
+
+            if (await _userRepository.SaveAllAsync())
+            {
+                return CreatedAtRoute("GetUser", new {username=user.UserName},_mapper.Map<PhotoDto>(photo));
+            }
+
+            return BadRequest("Problem adding photo");
+
+        }
     }
-}
 }
