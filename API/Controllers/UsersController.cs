f0630f4eeff0910a69874378a94054e1b41b4e0e
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-12-07 15:10:29 +0400

    refactor: update UsersController

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -19,41 +21,72 @@ namespace API.Controllers
     {
         private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
-        public UsersController(IUserRepository userRepository, IMapper mapper)
+        private readonly IPhotoService _photoService;
+        public UsersController(IUserRepository userRepository, IMapper mapper,
+        IPhotoService photoService)
         {
+            _photoService = photoService;
             _mapper = mapper;
             _userRepository = userRepository;
         }
 
 
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
-        {
-             var users = await _userRepository.GetMembersAsync();
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+    {
+        var users = await _userRepository.GetMembersAsync();
 
-            return Ok(users);
-        }
-
-
-        [HttpGet("{username}")]
-        public async Task<ActionResult<MemberDto>> GetUser(string username)
-        {
-            return await _userRepository.GetMemberAsync(username);
-        }
-
-        [HttpPut]
-        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
-        {
-            var username=User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            var user=await _userRepository.GetUserByUsernameAsync(username);
-
-            _mapper.Map(memberUpdateDto, user);
-
-            _userRepository.Update(user);
-
-            if(await _userRepository.SaveAllAsync()) return NoContent();
-
-            return BadRequest("Failed to update User");
-        }
+        return Ok(users);
     }
+
+
+    [HttpGet("{username}")]
+    public async Task<ActionResult<MemberDto>> GetUser(string username)
+    {
+        return await _userRepository.GetMemberAsync(username);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+        var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+
+        _mapper.Map(memberUpdateDto, user);
+
+        _userRepository.Update(user);
+
+        if (await _userRepository.SaveAllAsync()) return NoContent();
+
+        return BadRequest("Failed to update User");
+    }
+
+    [HttpPost("add-photo")]
+    public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
+    {
+        var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+
+        var result=await _photoService.AddPhotoAsync(file);
+
+        if(result.Error!=null) return BadRequest(result.Error.Message);
+
+        var photo=new Photo
+        {
+            Url=result.SecureUrl.AbsoluteUri,
+            PublicId=result.PublicId
+        };
+
+        if(user.Photos.Count==0)
+        {
+            photo.IsMain=true;
+        }
+
+        user.Photos.Add(photo);
+
+        if(await _userRepository.SaveAllAsync())
+            return _mapper.Map<PhotoDto>(photo);
+
+        return BadRequest("Problem adding photo");
+
+    }
+}
 }
