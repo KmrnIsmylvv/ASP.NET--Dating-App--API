7cd35a65962207cdb9be743cf5b582ae7e21dd41
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-12-08 05:51:17 +0400

    feat: setting the Main Photo

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -88,7 +88,24 @@ namespace API.Controllers
             }
 
             return BadRequest("Problem adding photo");
+        }
 
+        [HttpPut("set-main-photo/{photoId}")]
+        public async Task<ActionResult> SetMainPhoto(int photoId)
+        {
+            var user=await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+
+            var photo=user.Photos.FirstOrDefault(x=> x.Id==photoId);
+
+            if(photo.IsMain) return BadRequest("This is already your main photo");
+
+            var currentMain=user.Photos.FirstOrDefault(x=> x.IsMain);
+            if(currentMain!=null) currentMain.IsMain=false;
+            photo.IsMain=true;
+
+            if(await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to set main photo");
         }
     }
 }
