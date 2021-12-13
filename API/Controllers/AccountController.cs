f10f0f8e1aae97580c94bb5fdc59dcb3fdefbe8d
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-12-13 14:07:18 +0400

    feat: filtering users by created time

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -47,7 +47,8 @@ namespace API.Controllers
             {
                 UserName = user.UserName,
                 Token = _tokenService.CreateToken(user),
-                KnownAs=user.KnownAs
+                KnownAs=user.KnownAs,
+                Gender=user.Gender
             };
         }
 
@@ -74,7 +75,8 @@ namespace API.Controllers
                 UserName = user.UserName,
                 Token = _tokenService.CreateToken(user),
                 PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
-                KnownAs=user.KnownAs
+                KnownAs=user.KnownAs,
+                Gender=user.Gender
             };
         }
 
