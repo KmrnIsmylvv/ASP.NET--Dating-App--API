4df3568004ec1d4243ae1c02f9ca12b70a4a20e1
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-12-13 05:50:18 +0400

    feat: added helper classes for pagination

---
+++ API/Extensions/HttpExtensions.cs
@@ -0,0 +1,17 @@
+using System.Text.Json;
+using API.Helpers;
+using Microsoft.AspNetCore.Http;
+
+namespace API.Extensions
+{
+    public static class HttpExtensions
+    {
+        public static void AddPaginationHeader(this HttpResponse response, int currentPage,
+            int itemsPerPage, int totalItems, int totalPages)
+        {
+            var paginationHeader = new PaginationHeader(currentPage, itemsPerPage, totalItems, totalPages);
+            response.Headers.Add("Pagination", JsonSerializer.Serialize(paginationHeader));
+            response.Headers.Add("Acces-Control-Expose-Headers", "Pagination");
+        }
+    }
+}
---
+++ API/Helpers/UserParams.cs
@@ -0,0 +1,17 @@
+namespace API.Helpers
+{
+    public class UserParams
+    {
+        private const int MaxPageSize = 50;
+        public int PageNumber { get; set; } = 1;
+        private int _pageSize = 10;
+        public int Pagesize
+        {
+            get => _pageSize;
+            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+        }
+
+
+
+    }
+}
