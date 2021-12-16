8e0817d78d8629b860c6888d32132b9eebc20961
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-12-16 21:58:08 +0400

    refactor: making the Last Active action filter more optimal

--- API/Helpers/LogUserActivity.cs
+++ API/Helpers/LogUserActivity.cs
@@ -14,9 +14,9 @@ namespace API.Helpers
 
             if(!resultContext.HttpContext.User.Identity.IsAuthenticated) return;
 
-            var username=resultContext.HttpContext.User.GetUsername();
+            var userId=resultContext.HttpContext.User.GetUserId();
             var repo=resultContext.HttpContext.RequestServices.GetService<IUserRepository>();
-            var user=await repo.GetUserByUsernameAsync(username);
+            var user=await repo.GetUserByIdAsync(userId);
             user.LastActive=DateTime.Now;
             await repo.SaveAllAsync();
         }
