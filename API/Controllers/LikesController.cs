c2bdc58906e27f2f4ecb85dc9975bb1291816b8e
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-12-21 22:42:51 +0400

    feat: created Likes controller

---
+++ API/Controllers/LikesController.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Authorize]
+    public class LikesController : BaseApiController
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly ILikesRepository _likesRepository;
+        public LikesController(IUserRepository userRepository, ILikesRepository likesRepository)
+        {
+            _likesRepository = likesRepository;
+            _userRepository = userRepository;
+        }
+
+        [HttpPost("{username}")]
+        public async Task<ActionResult> AddLike(string username)
+        {
+            var sourceUserId = User.GetUserId();
+            var likedUser = await _userRepository.GetUserByUsernameAsync(username);
+            var sourceUser = await _likesRepository.GetUserWithLikes(sourceUserId);
+
+            if (likedUser == null) return NotFound();
+
+            if (sourceUser.UserName == username) return BadRequest("You cannot like yourself");
+
+            var userLike = await _likesRepository.GetUserLike(sourceUserId, likedUser.Id);
+
+            if (userLike != null) return BadRequest("You already like this user");
+
+            userLike = new UserLike
+            {
+                SourceUserId = sourceUserId,
+                LikedUserId = likedUser.Id
+            };
+
+            sourceUser.LikedUsers.Add(userLike);
+
+            if (await _userRepository.SaveAllAsync()) return Ok();
+
+            return BadRequest("Failed to like user");
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<LikeDto>>> GetUserLikes(string predicate)
+        {
+            var users = await _likesRepository.GetUserLikes(predicate, User.GetUserId());
+
+            return Ok(users);
+        }
+    }
+}
