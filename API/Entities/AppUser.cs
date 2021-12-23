adb025f76261babfcdc03195aaeb807fb7c6018b
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-12-23 06:55:06 +0400

    feat: created Messages Entity

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -6,24 +6,26 @@ namespace API.Entities
 {
     public class AppUser
     {
-        public int Id  { get; set; }
+        public int Id { get; set; }
         public string UserName { get; set; }
-        public byte[] PasswordHash  { get; set; }
-        public byte[] PasswordSalt  { get; set; }     
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
 
         public DateTime DateOfBirth { get; set; }
         public string KnownAs { get; set; }
-        public DateTime Created { get; set; }=DateTime.Now;
-        public DateTime LastActive { get; set; }=DateTime.Now;
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime LastActive { get; set; } = DateTime.Now;
         public string Gender { get; set; }
         public string Introduction { get; set; }
         public string LookingFor { get; set; }
         public string Interests { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
-        public ICollection<Photo> Photos { get; set;}
+        public ICollection<Photo> Photos { get; set; }
 
         public ICollection<UserLike> LikedByUsers { get; set; }
         public ICollection<UserLike> LikedUsers { get; set; }
+        public ICollection<Message> MessagesSent { get; set; }
+        public ICollection<Message> MessagesReceived { get; set; }
     }
 }
