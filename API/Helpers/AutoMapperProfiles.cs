edec7bf2f54cce19cf0c0a18dc9c28202c9c0371
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-12-25 20:07:54 +0400

    feat: added AutoMapper for Message class

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -7,17 +7,22 @@ using AutoMapper;
 
 namespace API.Helpers
 {
-    public class AutoMapperProfiles: Profile
+    public class AutoMapperProfiles : Profile
     {
         public AutoMapperProfiles()
         {
-            CreateMap<AppUser,MemberDto>()
-                .ForMember(dest=> dest.PhotoUrl,opt => opt.MapFrom(src => 
-                    src.Photos.FirstOrDefault(x=> x.IsMain).Url))
-                .ForMember(dest =>dest.Age,opt=> opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
-            CreateMap<Photo,PhotoDto>();
-            CreateMap<MemberUpdateDto,AppUser>();
-            CreateMap<RegisterDto,AppUser>();
+            CreateMap<AppUser, MemberDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src =>
+                      src.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<Photo, PhotoDto>();
+            CreateMap<MemberUpdateDto, AppUser>();
+            CreateMap<RegisterDto, AppUser>();
+            CreateMap<Message, MessageDto>()
+                .ForMember(dest => dest.SenderPhotoUrl, opt => opt.MapFrom(src =>
+                         src.Sender.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(dest => dest.RecipientPhotoUrl, opt => opt.MapFrom(src =>
+                         src.Recipient.Photos.FirstOrDefault(x => x.IsMain).Url));
         }
     }
 }
