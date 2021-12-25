e85783dc818521d16d85787b3f4892e57c5e8c72
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-12-25 20:54:38 +0400

    feat: Created Message Controller

---
+++ API/Controllers/MessagesController.cs
@@ -0,0 +1,55 @@
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Authorize]
+    public class MessagesController : BaseApiController
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMessageRepository _messageRepository;
+        private readonly IMapper _mapper;
+        public MessagesController(IUserRepository userRepository, IMessageRepository messageRepository,
+         IMapper mapper)
+        {
+            _mapper = mapper;
+            _messageRepository = messageRepository;
+            _userRepository = userRepository;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<MessageDto>> CreateMessage(CreateMessageDto createMessageDto)
+        {
+            var username = User.GetUsername();
+
+            if (username == createMessageDto.RecipientUsername.ToLower())
+                return BadRequest("You cannot send messages to yourself");
+
+            var sender = await _userRepository.GetUserByUsernameAsync(username);
+            var recipient = await _userRepository.GetUserByUsernameAsync(createMessageDto.RecipientUsername);
+
+            if (recipient == null) return NotFound();
+
+            var message = new Message
+            {
+                Sender = sender,
+                Recipient = recipient,
+                SenderUsername = sender.UserName,
+                RecipientUsername = recipient.UserName,
+                Content = createMessageDto.Content
+            };
+
+            _messageRepository.AddMessage(message);
+
+            if(await _messageRepository.SaveAllAsync()) return Ok(_mapper.Map<MessageDto>(message));
+
+            return BadRequest("Failed to send message");
+        }
+    }
+}
