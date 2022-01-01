4be74108e5bf8a9672bfdd4f44e14fc7b8904db0
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-01-01 23:00:34 +0400

    feat: setting up entities for Role Management

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -33,12 +33,7 @@ namespace API.Controllers
 
             var user = _mapper.Map<AppUser>(registerDto);
 
-            using var hmac = new HMACSHA512();
-
             user.UserName = registerDto.Username.ToLower();
-            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
-            user.PasswordSalt = hmac.Key;
-
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
@@ -60,15 +55,6 @@ namespace API.Controllers
             .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
 
             if (user == null) return Unauthorized("Invalid username");
-
-            using var hmac = new HMACSHA512(user.PasswordSalt);
-
-            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-
-            for (int i = 0; i < computedHash.Length; i++)
-            {
-                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
-            }
 
             return new UserDto
             {
--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -12,22 +12,18 @@ namespace API.Data
     {
         public static async Task SeedUsers(DataContext context)
         {
-            if(await context.Users.AnyAsync()) return;
+            if (await context.Users.AnyAsync()) return;
 
-            var userData=await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
-            var users=JsonSerializer.Deserialize<List<AppUser>>(userData);
+            var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
+            var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
             foreach (var user in users)
             {
-                using var hmac=new HMACSHA512();
-
-                user.UserName=user.UserName.ToLower();
-                user.PasswordHash=hmac.ComputeHash(Encoding.UTF8.GetBytes("Pa$$w0rd"));
-                user.PasswordSalt=hmac.Key;
+                user.UserName = user.UserName.ToLower();
 
                 context.Users.Add(user);
             }
 
             await context.SaveChangesAsync();
-        }    
+        }
     }
 }
