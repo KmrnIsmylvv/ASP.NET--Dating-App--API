0d18128f5e6b9c92e44e09ec295a928eaf97cbf9
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-01-07 17:07:04 +0400

    refactor: updating Seed Method

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -1,18 +1,17 @@
 using System.Collections.Generic;
-using System.Security.Cryptography;
-using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
     public class Seed
     {
-        public static async Task SeedUsers(DataContext context)
+        public static async Task SeedUsers(UserManager<AppUser> userManager)
         {
-            if (await context.Users.AnyAsync()) return;
+            if (await userManager.Users.AnyAsync()) return;
 
             var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
@@ -20,10 +19,8 @@ namespace API.Data
             {
                 user.UserName = user.UserName.ToLower();
 
-                context.Users.Add(user);
+                await userManager.CreateAsync(user, "Pa$$w0rd");
             }
-
-            await context.SaveChangesAsync();
         }
     }
 }
