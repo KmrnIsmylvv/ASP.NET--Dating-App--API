c7ecfb02df0c4d7fdf100c3d8af189fe8014d6ff
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-01-19 14:37:21 +0400

    feat: Notifying users when they receive a message

--- API/SignalR/MessageHub.cs
+++ API/SignalR/MessageHub.cs
@@ -15,9 +15,14 @@ namespace API.SignalR
         private readonly IMessageRepository _messageRepository;
         private readonly IMapper _mapper;
         private readonly IUserRepository _userRepository;
+        private readonly IHubContext<PresenceHub> _presenceHub;
+        private readonly PresenceTracker _tracker;
         public MessageHub(IMessageRepository messageRepository, IMapper mapper,
-            IUserRepository userRepository)
+        IUserRepository userRepository, IHubContext<PresenceHub> presenceHub,
+        PresenceTracker tracker)
         {
+            _tracker = tracker;
+            _presenceHub = presenceHub;
             _userRepository = userRepository;
             _mapper = mapper;
             _messageRepository = messageRepository;
@@ -70,8 +75,17 @@ namespace API.SignalR
 
             if (group.Connections.Any(x => x.Username == recipient.UserName))
             {
-                message.DateRead=DateTime.UtcNow;
-            }   
+                message.DateRead = DateTime.UtcNow;
+            }
+            else
+            {
+                var connections = await _tracker.GetConnectionsForUser(recipient.UserName);
+                if (connections != null)
+                {
+                    await _presenceHub.Clients.Clients(connections).SendAsync("NewMessageReceived",
+                    new { username = sender.UserName, knownAs = sender.KnownAs });
+                }
+            }
 
             _messageRepository.AddMessage(message);
 
